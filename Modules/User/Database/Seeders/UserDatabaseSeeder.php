7c8ea716b684f2b6554a6f4b85953fc3142b1cc1
Taveevut Nakomah <nakomah.web@gmail.com>
2020-07-22 22:51:08 +0700

    Make factory && seeding user data 10 record
    php artisan module:make-factory UserFactory User
    php artisan module:seed User

--- Modules/User/Database/Seeders/UserDatabaseSeeder.php
+++ Modules/User/Database/Seeders/UserDatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Modules\User\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Modules\User\Entities\User;
 
 class UserDatabaseSeeder extends Seeder
 {
@@ -16,6 +17,6 @@ class UserDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call("OthersTableSeeder");
+        factory(User::class, 10)->create();
     }
 }
