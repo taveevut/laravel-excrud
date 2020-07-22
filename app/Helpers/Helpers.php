b5fd8176abebb8e436615533ea0b64a070afbe32
Taveevut Nakomah <nakomah.web@gmail.com>
2020-07-23 00:55:58 +0700

    Add function site_string_url

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -22,3 +22,18 @@ if (!function_exists('breadcrumb')) {
         return $result;
     }
 }
+
+if (!function_exists('site_string_url')) {
+    function site_string_url($string = '')
+    {
+        $string = preg_replace("`\[.*\]`U", "", $string);
+        $string = preg_replace('`&(amp;)?#?[a-z0-9]+;`i', '-', $string);
+        $string = str_replace('%', '-percent', $string);
+        $string = htmlentities($string, ENT_COMPAT, 'utf-8');
+        $string = preg_replace("`&([a-z])(acute|uml|circ|grave|ring|cedil|slash|tilde|caron|lig|quot|rsquo);`i", "\\1", $string);
+        $string = preg_replace(["`[^a-z0-9ก-๙เ-า]`i", "`[-]+`"], "-", $string);
+        $response = strtolower(trim($string, '-'));
+
+        return $response;
+    }
+}
